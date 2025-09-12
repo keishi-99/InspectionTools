7322e07d06479c472fd2620b64dca5c7c65bdfc6
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2025-09-12 15:15:46 +0900

    EL0122FI 完成

---
+++ EL0122FI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EL0122FI {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+
+}
