513768736ea0998bc080ba39b9c29d18600bb22a
Keishi Tokudome <Gideon_apple@yahoo.co.jp>
2025-09-17 10:46:13 +0900

    EL9100 完成

---
+++ EL9100/MainWindow.xaml.cs
@@ -0,0 +1,413 @@
+﻿using System.Collections.ObjectModel;
+using System.Data;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using Tools.Common;
+using Tools.Common.InstList;
+using WindowsInput;
+using static Tools.Common.Win32Wrapper;
+
+namespace EL9100 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private IntPtr _hWnd = IntPtr.Zero;
+
+        private readonly InstClass _instDmm01;
+        private readonly InstClass _instDmm02;
+
+        public ObservableCollection<string> Dmm1List { get; } = [];
+        public ObservableCollection<string> Dmm2List { get; } = [];
+
+        public MainWindow() {
+            InitializeComponent();
+            _instDmm01 = new();
+            _instDmm02 = new();
+            LoadEvents();
+            // Window が完全に作られたあとにハンドルを取得
+            Loaded += (s, e) => { _hWnd = new WindowInteropHelper(this).Handle; };
+        }
+
+        public class InstClass {
+            internal USBDeviceManager UsbDev { get; set; } = new();
+            public string Category { get; set; } = string.Empty;
+            public string Name { get; set; } = string.Empty;
+            public string VisaAddress { get; set; } = string.Empty;
+            public int SignalType { get; set; } = 0;
+            public int Index { get; set; } = 0;
+            public string InstCommand { get; set; } = string.Empty;
+            public int SettingNumber { get; set; } = 0;
+
+            public void ResetProperties() {
+                UsbDev = new();
+                Name = string.Empty;
+                Category = string.Empty;
+                VisaAddress = string.Empty;
+                SignalType = 0;
+                Index = 0;
+                InstCommand = string.Empty;
+                SettingNumber = 0;
+            }
+            public void Dispose() {
+                // UsbDevの解放処理
+                UsbDev?.Dispose();
+            }
+        }
+
+        private const int TimeOut = 3;    //タイムアウトまでの時間(sec)
+
+        internal DataTable _dataTable = new();
+
+        private readonly List<Hotkey> _hotkeys = [];
+        private HwndSource? _source;
+
+        private volatile bool _isProcessing = false;
+
+        private static readonly SemaphoreSlim s_semaphore = new(1, 1); // 最大1つの接続
+
+        // 起動時
+        private void LoadEvents() {
+            InstListImport();
+            FormatSet();
+        }
+        private void InstListImport() {
+            const string XmlFilePath = "VisaAddress.xml";
+            if (!System.IO.File.Exists(XmlFilePath)) {
+                MessageBox.Show($"{XmlFilePath}が見つかりません。");
+                return;
+            }
+
+            using DataSet dataSet = new();
+            dataSet.ReadXml("VisaAddress.xml");
+            _dataTable = dataSet.Tables[0];
+
+            // デジタルマルチメータ、ファンクションジェネレータ、オシロスコープのコンボボックスを更新する
+            UpdateComboBox(Dmm01ComboBox, Dmm1List, "デジタルマルチメータ", [1, 2], "[DMM1]");
+            UpdateComboBox(Dmm02ComboBox, Dmm2List, "デジタルマルチメータ", [1, 2], "[DMM2]");
+        }
+        private void UpdateComboBox(ComboBox comboBox, ObservableCollection<string> collection, string category, List<int> signalTypes, string name) {
+            if (_dataTable == null) return;
+            collection.Clear();
+            collection.Add(name);
+
+            foreach (var signalType in signalTypes) {
+                var rows = _dataTable.Select($"Category = '{category}' AND SignalType = {signalType}");
+                foreach (var d in rows) {
+                    collection.Add(d["Name"].ToString() ?? string.Empty);
+                }
+            }
+            comboBox.ItemsSource = collection;
+        }
+        // 処理中の画像を表示/非表示にします。
+        private void VisibleProgressImage(bool isVisible) {
+            _isProcessing = isVisible;
+            MainGrid.IsEnabled = !isVisible;
+            ProgressRing.Visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        // 選択した機器のVisaAddressを取得
+        private void SelectInst() {
+            GetVisaAddress(_instDmm01, Dmm01ComboBox);
+            GetVisaAddress(_instDmm02, Dmm02ComboBox);
+        }
+        private void GetVisaAddress(InstClass instClass, ComboBox comboBox) {
+            instClass.ResetProperties();
+
+            instClass.Name = comboBox.Text;
+            instClass.Index = comboBox.SelectedIndex;
+
+            if (instClass.Index <= 0) { return; }
+
+            var dRows = _dataTable.Select($"Name = '{instClass.Name}'");
+            instClass.Category = dRows[0]["Category"] as string ?? string.Empty;
+            instClass.VisaAddress = dRows[0]["VisaAddress"] as string ?? string.Empty;
+            instClass.SignalType = dRows[0]["SignalType"] != DBNull.Value ? Convert.ToInt32(dRows[0]["SignalType"]) : 0;
+        }
+        // 機器リスト表示
+        private void ShowInstList() {
+            InstListWindow frm1 = new(_dataTable);
+            frm1.ShowDialog();
+            InstListImport();
+        }
+        // 機器初期設定
+        private void FormatSet() {
+            _instDmm01.InstCommand = _instDmm01.SignalType switch {
+                1 => "*RST,F5,R6,*OPC?",
+                2 => "*RST;:INIT:CONT 1;:CONF:CURR:DC;:CURR:DC:RANG 0.02;*OPC?",
+                _ => string.Empty,
+            };
+            _instDmm02.InstCommand = _instDmm02.SignalType switch {
+                1 => "*RST,F5,R8,*OPC?",
+                2 => "*RST;:INIT:CONT 1;:CONF:CURR:DC;:CURR:DC:RANG 10;*OPC?",
+                _ => string.Empty,
+            };
+        }
+
+        // 機器接続
+        private async void ConnectInstAsync() {
+            try {
+                HotKeyChekBox.IsChecked = false;
+                VisibleProgressImage(true);
+
+                SelectInst();
+                CheckDmmId();
+                FormatSet();
+
+                var devices = new[] { _instDmm01, _instDmm02 };
+                var tasks = devices.Select(device => ConnectDeviceAsync(device));
+                await Task.WhenAll(tasks);
+
+
+                Dmm01ComboBox.IsEnabled = false;
+                Dmm02ComboBox.IsEnabled = false;
+                ConnectButton.IsEnabled = false;
+                ReleaseButton.IsEnabled = true;
+                InstListButton.IsEnabled = false;
+
+            } catch (Exception ex) {
+                Release();
+                MessageBox.Show(ex.Message, "エラー");
+            } finally {
+                VisibleProgressImage(false);
+            }
+        }
+        // DMMのIDチェック処理
+        private void CheckDmmId() {
+            var indices = new[] { _instDmm01.Index, _instDmm02.Index }
+                .Where(i => i >= 1); // 未選択(0以下)は無視
+
+            if (indices.Count() != indices.Distinct().Count()) {
+                throw new Exception("同じ測定器が選択されています。");
+            }
+        }
+        // デバイス接続
+        private static async Task<string> ConnectDeviceAsync(InstClass instClass) {
+            return instClass.Index < 1
+                ? ""
+                : instClass.SignalType switch {
+                    1 => await ConnectDeviceAdcAsync(instClass),
+                    2 or 3 or 4 => await ConnectDeviceVisaAsync(instClass),
+                    _ => throw new ApplicationException(),
+                };
+        }
+        // Visa接続
+        private static async Task<string> ConnectDeviceVisaAsync(InstClass instClass) {
+            return await Task.Run(() => {
+                using var usbDev = new USBDeviceManager();
+                usbDev.OpenDev(instClass.VisaAddress);
+                usbDev.OutputDev(instClass.InstCommand);
+                return usbDev.InputDev();
+            });
+        }
+        // ADC接続
+        private static async Task<string> ConnectDeviceAdcAsync(InstClass instClass) {
+            await s_semaphore.WaitAsync();
+            try {
+                uint hDev = 0;
+                var rcvDt = "";
+                uint rcvLen = 50;
+                var id = uint.Parse(instClass.VisaAddress);
+                try {
+                    if (AusbWrapper.Start(TimeOut) != 0 || AusbWrapper.Open(ref hDev, id) != 0) { throw new Exception("開始できません"); }
+                    if (!string.IsNullOrEmpty(instClass.InstCommand)) {
+                        if (AusbWrapper.Write(hDev, instClass.InstCommand) != 0) { throw new Exception("コマンドの送信に失敗しました"); }
+                    }
+                    if (AusbWrapper.Read(hDev, ref rcvDt, ref rcvLen) != 0) { throw new Exception("メッセージの受信に失敗しました"); }
+                } finally {
+                    _ = AusbWrapper.Close(hDev);
+                    _ = AusbWrapper.End();
+                }
+                return rcvDt;
+            } finally {
+                s_semaphore.Release();
+            }
+        }
+
+        // 解除
+        private void Release() {
+            VisibleProgressImage(false);
+
+            _instDmm01.ResetProperties();
+            _instDmm02.ResetProperties();
+
+            Dmm01ComboBox.IsEnabled = true;
+            Dmm02ComboBox.IsEnabled = true;
+            ConnectButton.IsEnabled = true;
+            ReleaseButton.IsEnabled = false;
+            InstListButton.IsEnabled = true;
+            HotKeyChekBox.IsChecked = false;
+        }
+
+        // DMM測定値取得
+        private async Task<decimal> ReadDmm(InstClass instClass) {
+            try {
+                VisibleProgressImage(true);
+
+                instClass.InstCommand = instClass.SignalType switch {
+                    1 => string.Empty,
+                    2 => "FETC?",
+                    _ => throw new ApplicationException(),
+                };
+
+                var result = await ConnectDeviceAsync(instClass);
+                decimal.TryParse(result, NumberStyles.AllowExponent | NumberStyles.Float, CultureInfo.InvariantCulture, out var output);
+
+                return output;
+
+            } finally {
+                VisibleProgressImage(false);
+            }
+        }
+        // DMM切り替え
+        private async Task SwitchDmm2(InstClass instClass, string func) {
+            try {
+                VisibleProgressImage(true);
+
+                instClass.InstCommand = func switch {
+                    "DCI" => instClass.SignalType switch {
+                        1 => "*RST,F5,R8,*OPC?",
+                        2 => "*RST;:INIT:CONT 1;:CONF:CURR:DC;:CURR:DC:RANG 10;*OPC?",
+                        _ => throw new ApplicationException(),
+                    },
+                    "DCV" => instClass.SignalType switch {
+                        1 => "*RST,F1,R6,*OPC?",
+                        2 => "*RST;:INIT:CONT 1;:VOLT:DC:RANG 20;*OPC?",
+                        _ => throw new ApplicationException(),
+                    },
+                    _ => throw new ApplicationException(),
+                };
+
+                await ConnectDeviceAsync(instClass);
+
+            } finally {
+                VisibleProgressImage(false);
+            }
+        }
+
+        // DMM01測定値コピー
+        private async void ActionHotkeyBracketR() {
+            if (_isProcessing) { return; }
+
+            try {
+                var output = await ReadDmm(_instDmm01);
+
+                var sim = new InputSimulator();
+                sim.Keyboard.TextEntry((output * 1000000).ToString("0"));
+                await Task.Delay(100);
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            } catch (Exception ex) {
+                Release();
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        // DMM02測定値コピー
+        private async void ActionHotkeySlash() {
+            if (_isProcessing) { return; }
+
+            try {
+                var output = await ReadDmm(_instDmm02);
+
+                var sim = new InputSimulator();
+                sim.Keyboard.TextEntry((output * 1000).ToString("0.0"));
+                await Task.Delay(100);
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            } catch (Exception ex) {
+                Release();
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        private async void ActionHotkeyBackslash() {
+            if (_isProcessing) { return; }
+
+            try {
+                var output = await ReadDmm(_instDmm02);
+
+                var sim = new InputSimulator();
+                sim.Keyboard.TextEntry((output).ToString("0.00"));
+                await Task.Delay(100);
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            } catch (Exception ex) {
+                Release();
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        // DMM2切替(DCI)
+        private async void ActionHotkeyAtsign() {
+            if (_isProcessing) { return; }
+
+            try {
+                await SwitchDmm2(_instDmm02, "DCI");
+            } catch (Exception ex) {
+                Release();
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        // DMM2切替(DCV)
+        private async void ActionHotkeyBracketL() {
+            if (_isProcessing) { return; }
+
+            try {
+                await SwitchDmm2(_instDmm02, "DCV");
+            } catch (Exception ex) {
+                Release();
+                MessageBox.Show(ex.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        // HotkKeyの登録
+        private void SetHotKey() {
+            _hotkeys.Clear();
+            if (!string.IsNullOrEmpty(_instDmm01.VisaAddress)) {
+                _hotkeys.AddRange([
+                    new(ModNone, HotkeyBracketR, ActionHotkeyBracketR),
+                ]);
+            }
+            if (!string.IsNullOrEmpty(_instDmm02.VisaAddress)) {
+                _hotkeys.AddRange([
+                    new(ModNone, HotkeySlash, ActionHotkeySlash),
+                    new(ModNone, HotkeyBackslash, ActionHotkeyBackslash),
+                    new(ModNone, HotkeyAtsign, ActionHotkeyAtsign),
+                    new(ModNone, HotkeyBracketL, ActionHotkeyBracketL ),
+                ]);
+            }
+
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+
+            // ホットキーを登録
+            foreach (var hotkey in _hotkeys) {
+                RegisterHotKey(_hWnd, hotkey.Id, hotkey.Modifier, (uint)hotkey.VirtualKey);
+            }
+        }
+        private void ClearHotKey() {
+            foreach (var hotkey in _hotkeys) {
+                UnregisterHotKey(_hWnd, hotkey.Id);
+            }
+            _hotkeys.Clear();
+        }
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
+            if (msg == WmHotKey) {
+                int id = wParam.ToInt32();
+
+                var hotkey = _hotkeys.FirstOrDefault(h => h.Id == id);
+                hotkey?.Action.Invoke(); // ホットキーに設定されたアクションを実行
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e) { ConnectInstAsync(); }
+        private void ReleaseButton_Click(object sender, RoutedEventArgs e) { Release(); }
+        private void InstListButton_Click(object sender, RoutedEventArgs e) { ShowInstList(); }
+        private void HotKeyChekBox_Checked(object sender, RoutedEventArgs e) { SetHotKey(); }
+        private void HotKeyChekBox_Unchecked(object sender, RoutedEventArgs e) { ClearHotKey(); }
+        private void TopMostCheckBox_Checked(object sender, RoutedEventArgs e) { Topmost = true; }
+        private void TopMostCheckBox_Unchecked(object sender, RoutedEventArgs e) { Topmost = false; }
+
+    }
+}
