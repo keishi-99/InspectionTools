158687901f629b918fc1e546a016eb3e6596b699
Keishi Tokudome <Gideon_apple@yahoo.co.jp>
2025-10-21 16:50:50 +0900

    不要コメント削除

--- InspectionTools/Common/HelpManager.cs
+++ InspectionTools/Common/HelpManager.cs
@@ -4,62 +4,6 @@ using System.Text.Json;
 namespace InspectionTools.Common {
     public static class HelpManager {
 
-        //public class HelpEntry {
-        //    public string Key { get; set; } = string.Empty;
-        //    public string Description { get; set; } = string.Empty;
-        //}
-
-        //private static Dictionary<string, List<HelpEntry>>? s_helpData;
-
-
-        //public static void LoadHelpFile(string path) {
-        //    if (!File.Exists(path)) {
-        //        s_helpData = [];
-        //        return;
-        //    }
-
-        //    string json = File.ReadAllText(path);
-        //    s_helpData = JsonSerializer.Deserialize<Dictionary<string, List<HelpEntry>>>(json);
-        //}
-
-        //public static List<HelpEntry> GetHelpList(string key) {
-        //    if (s_helpData == null) {
-        //        throw new InvalidOperationException("Helpファイルが読み込まれていません。");
-        //    }
-
-        //    return s_helpData.TryGetValue(key, out var list)
-        //        ? list
-        //        : [new() { Key = "なし", Description = "ヘルプ情報が登録されていません。" }];
-        //}
-
-
-        //    private static Dictionary<string, string[]>? s_helpTexts;
-
-        //    // JSONファイルの読み込み
-        //    public static void LoadHelpFile(string path) {
-        //        if (!File.Exists(path)) {
-        //            s_helpTexts = [];
-        //            return;
-        //        }
-
-        //        string json = File.ReadAllText(path);
-        //        s_helpTexts = JsonSerializer.Deserialize<Dictionary<string, string[]>>(json);
-        //    }
-
-        //    // 指定キーのヘルプテキストを取得
-        //    public static string GetHelpText(string key) {
-        //        if (s_helpTexts == null) {
-        //            throw new InvalidOperationException("HelpManager: JSONファイルが読み込まれていません。");
-        //        }
-
-        //        if (!s_helpTexts.TryGetValue(key, out var lines) || lines.Length == 0) {
-        //            return "ヘルプ情報が登録されていません。";
-        //        }
-
-        //        return string.Join(Environment.NewLine, lines);
-        //    }
-        //}
-
         private static Dictionary<string, (string[] Keys, string[] Descriptions)>? s_helpTexts;
 
         // JSONファイルの読み込み
