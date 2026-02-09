4606184092ce3d7cdc1f78cd223cbde26ea36d49
Keishi Tokudome <keishi.tokudome.uw@oval.co.jp>
2026-02-09 16:46:39 +0900

    EL9220追加

--- InspectionTools/Menu/MainMenuUserControl.xaml.cs
+++ InspectionTools/Menu/MainMenuUserControl.xaml.cs
@@ -20,6 +20,7 @@ namespace InspectionTools.MainMenu {
         private void EL4001_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("EL4001"); }
         private void EL5000_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("EL5000"); }
         private void EL9100_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("EL9100"); }
+        private void EL9220_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("EL9220"); }
         private void EL9230_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("EL9230"); }
         private void EL9240_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("EL9240"); }
         private void PA14_Click(object sender, RoutedEventArgs e) { PageSelected?.Invoke("PA14"); }
